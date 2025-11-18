35f0fee6a9182e770916e5fc6551d95412cae351
ialina07 <alinaiva07@mail.ru>
2025-11-18 21:52:28 +0000

    added correction of mistakes for homework Sorted List

--- src/homework6.1_28.10_sortedList/sortedList.c
+++ src/homework6.1_28.10_sortedList/sortedList.c
@@ -1,135 +1,221 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
+// Структура узла списка
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
-int sortedList[MAX_SIZE];
-int listSize = 0;
+// Структура отсортированного списка
+typedef struct {
+    Node* head;
+    int size;
+} SortedList;
 
-void addValue();
-void removeValue();
-void printList();
+// Прототипы функций
+SortedList* createSortedList();
+void destroySortedList(SortedList* list);
+void addValue(SortedList* list, int value);
+void removeValue(SortedList* list, int value);
+void printList(const SortedList* list);
+int getListSize(const SortedList* list);
 
 int main()
 {
+    SortedList* myList = createSortedList();
     int choice;
 
     while (1) {
-        printf("\nДоступные операции:\n");
+        printf("\nОперации:\n");
         printf("0 - выйти\n");
-        printf("1 - добавить значение в отсортированный список\n");
+        printf("1 - добавить значение в сортированный список\n");
         printf("2 - удалить значение из списка\n");
         printf("3 - распечатать список\n");
 
-        printf("Выберете операцию (0-3): ");
+        printf("Выбор: ");
         if (scanf("%d", &choice) != 1) {
-            printf("Ошибка: введите число от 0 до 3\n");
-            while (getchar() != '\n'); // очистка буфера ввода
+            printf("Ошибка ввода!\n");
+            while(getchar() != '\n');
             continue;
         }
 
-        switch (choice) {
-        case 0:
-            printf("Выход из программы\n");
-            return 0;
-        case 1:
-            addValue();
-            break;
-        case 2:
-            removeValue();
-            break;
-        case 3:
-            printList();
-            break;
-        default:
-            ("Ошибка: неверный выбор операции\n");
+        switch(choice) {
+            case 0:
+                destroySortedList(myList);
+                printf("Программа завершена\n");
+                return 0;
+            case 1: {
+                int value;
+                printf("Введите число: ");
+                if (scanf("%d", &value) != 1) {
+                    printf("Ошибка: введите число!\n");
+                    while (getchar() != '\n');
+                    break;
+                }
+                addValue(myList, value);
+                break;
+            }
+            case 2: {
+                int value;
+                printf("Удалить число: ");
+                if (scanf("%d", &value) != 1) {
+                    printf("Ошибка: введите число!\n");
+                    while (getchar() != '\n');
+                    break;
+                }
+                removeValue(myList, value);
+                break;
+            }
+            case 3:
+                printList(myList);
+                break;
+            default:
+                printf("Неверный выбор!\n");
         }
     }
-
-    return 0;
 }
 
-void addValue()
+// Создание нового отсортированного списка
+SortedList* createSortedList()
 {
-    if (listSize >= MAX_SIZE) {
-        printf("Ошибка: список полон\n");
-        return;
+    SortedList* list = (SortedList*)malloc(sizeof(SortedList));
+    if (list == NULL) {
+        printf("Ошибка создания списка!\n");
+        return NULL;
     }
-
-    int value;
-    printf("Введите значение для добавления: ");
-    if (scanf("%d", &value) != 1) {
-        printf("Ошибка: введите целое число");
-        while (getchar() != '\n'); // очистка буфера ввода
-        return;
-    }
-
-    // находим позицию для вставки
-    int pos = 0;
-    while (pos < listSize && sortedList[pos] < value) {
-        pos++;
-    }
-
-    // сдвигаем элементы вправо
-    for (int i = listSize; i > pos; i--)
-        sortedList[i] = sortedList[i - 1];
-
-    // вставляем значение на найденную позицию
-    sortedList[pos] = value;
-    listSize++;
-
-    printf("Значение %d добавлено в список\n", value);
+    list->head = NULL;
+    list->size = 0;
+    return list;
 }
 
-void removeValue()
+// Уничтожение списка и освобождение памяти
+void destroySortedList(SortedList* list)
 {
-    if (listSize == 0) {
-        printf("Список пуст. Удалять нечего");
+    if (list == NULL) return;
+
+    Node* current = list->head;
+    while (current != NULL) {
+        Node* nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+
+    free(list);
+    printf("Память освобождена\n");
+}
+
+// Добавление значения в отсортированный список
+void addValue(SortedList* list, int value)
+{
+    if (list == NULL) {
+        printf("Ошибка: список не существует!\n");
         return;
     }
 
-    int value;
-    printf("Введите значение для удаления: ");
-    if (scanf("%d", &value) != 1) {
-        printf("Ошибка: введите целое число");
-        while (getchar() != '\n'); // очистка буфера ввода
+    // Создаем новый узел
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Ошибка памяти!\n");
+        return;
+    }
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // Если список пустой
+    if (list->head == NULL) {
+        list->head = newNode;
+        list->size++;
+        printf("Добавлено: %d\n", value);
         return;
     }
 
-    // ищем значение в списке
-    int found = 0;
-    int pos = -1;
-    for (int i = 0; i < listSize; i++) {
-        if (sortedList[i] == value) {
-            found = 1;
-            pos = i;
-            break;
-        }
+    // Если вставляем в начало
+    if (value < list->head->data) {
+        newNode->next = list->head;
+        list->head = newNode;
+        list->size++;
+        printf("Добавлено: %d\n", value);
+        return;
     }
 
-    if (found) {
-        // сдвигаем элементы влево
-        for (int i = pos; i < listSize - 1; i++) {
-            sortedList[i] = sortedList[i + 1];
-        }
-        listSize--;
-        printf("Значение %d удалено из списка\n", value);
+    // Ищем место для вставки
+    Node* current = list->head;
+    Node* prev = NULL;
+
+    while (current != NULL && current->data < value) {
+        prev = current;
+        current = current->next;
+    }
+
+    // Вставляем между prev и current
+    if (prev != NULL) {
+        prev->next = newNode;
+    }
+    newNode->next = current;
+    list->size++;
+
+    printf("Добавлено: %d\n", value);
+}
+
+// Удаление значения из списка
+void removeValue(SortedList* list, int value)
+{
+    if (list == NULL || list->head == NULL) {
+        printf("Список пуст!\n");
+        return;
+    }
+
+    // Ищем элемент для удаления
+    Node* current = list->head;
+    Node* prev = NULL;
+
+    while (current != NULL && current->data != value) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Число %d не найдено!\n", value);
+        return;
+    }
+
+    // Удаляем элемент
+    if (prev == NULL) {
+        // Удаляем голову
+        list->head = current->next;
     } else {
-        printf("Значение %d не найдено в списке\n", value);
+        prev->next = current->next;
     }
+
+    free(current);
+    list->size--;
+    printf("Удалено: %d\n", value);
 }
 
-void printList()
+// Печать списка
+void printList(const SortedList* list)
 {
-    if (listSize == 0)
-        printf("Список пуст\n");
-    else {
-        printf("Текущий список: [");
-        for (int i = 0; i < listSize; i++) {
-            printf("%d", sortedList[i]);
-            if (i < listSize - 1)
-                printf(", ");
-        }
-        printf("]\n");
+    if (list == NULL || list->head == NULL) {
+        printf("Список пуст!\n");
+        return;
     }
+
+    printf("Список (%d элементов): ", list->size);
+    Node* current = list->head;
+    while (current != NULL) {
+        printf("%d", current->data);
+        if (current->next != NULL) {
+            printf(" -> ");
+        }
+        current = current->next;
+    }
+    printf("\n");
+}
+
+// Получение размера списка
+int getListSize(const SortedList* list)
+{
+    if (list == NULL) return 0;
+    return list->size;
 }
