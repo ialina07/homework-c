4f481fd3bfa3ec9a20406d38d2e0be5f98bc0bb6
ialina07 <alinaiva07@mail.ru>
2025-12-16 21:56:09 +0000

    fix format

--- tests/homework10/testSortedList.c
+++ tests/homework10/testSortedList.c
@@ -1,12 +1,15 @@
+#include "sortedList.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "sortedList.h"
 
 // Вспомогательная функция для проверки содержимого списка
-int isListEqual(const SortedList* list, int* expected, int expectedSize) {
-    if (list == NULL) return 0;
-    if (getListSize(list) != expectedSize) return 0;
+int isListEqual(const SortedList* list, int* expected, int expectedSize)
+{
+    if (list == NULL)
+        return 0;
+    if (getListSize(list) != expectedSize)
+        return 0;
 
     Node* current = list->head;
     for (int i = 0; i < expectedSize; i++) {
@@ -18,12 +21,14 @@ int isListEqual(const SortedList* list, int* expected, int expectedSize) {
     return current == NULL;
 }
 
-void printTestResult(const char* testName, int passed) {
+void printTestResult(const char* testName, int passed)
+{
     printf("  %s %s\n", passed ? "YES" : "NO", testName);
 }
 
 // Тесты
-void testCreateDestroy() {
+void testCreateDestroy()
+{
     printf("1. Создание и удаление списка\n");
 
     SortedList* list = createSortedList();
@@ -34,7 +39,8 @@ void testCreateDestroy() {
     printTestResult("Корректное удаление", 1);
 }
 
-void testAddValues() {
+void testAddValues()
+{
     printf("\n2. Добавление значений\n");
 
     SortedList* list = createSortedList();
@@ -45,14 +51,15 @@ void testAddValues() {
     addValue(list, 8);
     addValue(list, 1);
 
-    int expected[] = {1, 2, 5, 8};
+    int expected[] = { 1, 2, 5, 8 };
     int passed = isListEqual(list, expected, 4);
     printTestResult("Сортированное добавление", passed);
 
     destroySortedList(list);
 }
 
-void testRemoveValues() {
+void testRemoveValues()
+{
     printf("\n3. Удаление значений\n");
 
     SortedList* list = createSortedList();
@@ -63,26 +70,27 @@ void testRemoveValues() {
 
     // Удаляем из середины
     removeValue(list, 3);
-    int expected1[] = {1, 2, 4};
+    int expected1[] = { 1, 2, 4 };
     int passed1 = isListEqual(list, expected1, 3);
     printTestResult("Удаление из середины", passed1);
 
     // Удаляем голову
     removeValue(list, 1);
-    int expected2[] = {2, 4};
+    int expected2[] = { 2, 4 };
     int passed2 = isListEqual(list, expected2, 2);
     printTestResult("Удаление головы", passed2);
 
     // Удаляем хвост
     removeValue(list, 4);
-    int expected3[] = {2};
+    int expected3[] = { 2 };
     int passed3 = isListEqual(list, expected3, 1);
     printTestResult("Удаление хвоста", passed3);
 
     destroySortedList(list);
 }
 
-void testEmptyList() {
+void testEmptyList()
+{
     printf("\n4. Граничные случаи\n");
 
     SortedList* list = createSortedList();
@@ -101,7 +109,8 @@ void testEmptyList() {
     destroySortedList(list);
 }
 
-void testDuplicates() {
+void testDuplicates()
+{
     printf("\n5. Обработка дубликатов\n");
 
     SortedList* list = createSortedList();
@@ -111,14 +120,14 @@ void testDuplicates() {
     addValue(list, 5);
     addValue(list, 5);
 
-    int expected[] = {3, 5, 5};
+    int expected[] = { 3, 5, 5 };
     int passed = isListEqual(list, expected, 3);
     printTestResult("Добавление дубликатов", passed);
 
     // Удаляем один дубликат
     removeValue(list, 5);
 
-    int expected2[] = {3, 5};
+    int expected2[] = { 3, 5 };
     int passed2 = isListEqual(list, expected2, 2);
     printTestResult("Удаление дубликата", passed2);
 
@@ -126,7 +135,8 @@ void testDuplicates() {
 }
 
 // Главная функция тестов
-void runTests() {
+void runTests()
+{
     printf("ЗАПУСК ТЕСТОВ\n\n");
 
     testCreateDestroy();
@@ -138,7 +148,8 @@ void runTests() {
     printf("\nТЕСТЫ ЗАВЕРШЕНЫ\n");
 }
 
-int main() {
+int main()
+{
     runTests();
     return 0;
 }
